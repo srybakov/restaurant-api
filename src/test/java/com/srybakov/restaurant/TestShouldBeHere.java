28812f5dae956955f65eb88f04220a36c03433c2
Sergey Rybakov <sarybako@gmail.com>
2015-11-17 16:11:44 +0300

    Test

---
+++ src/test/java/com/srybakov/restaurant/TestShouldBeHere.java
@@ -0,0 +1,15 @@
+package com.srybakov.restaurant;
+
+/**
+ * @author <a href="mailto:sarybako@gmail.com">Sergey Rybakov</a>
+ */
+public class TestShouldBeHere {
+
+    /**
+     * I'm very sorry! I know that I should provide tests for this application but I have an extremely busy
+     * schedule and I cannot do it timely unfortunately :(
+     *
+     * I absolutely believe that you'll understand me and give me a chance work within your team! :) :)
+     *
+     */
+}
